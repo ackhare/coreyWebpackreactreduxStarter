f5c70df6f3caf1801ca1abbc5c49dd25d21baee8
Khare <Chetan.Khare@CenturyLink.com>
2019-02-14 15:35:24 +0530

    Integrated creative tim react app into coreey slingshot app with saas support without node-sass

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import NotFoundPage from "./NotFoundPage";
 import PropTypes from "prop-types";
 import React from "react";
 import { hot } from "react-hot-loader";
-
+import indexRoutes from "../routes/index.jsx";
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
@@ -18,18 +18,21 @@ class App extends React.Component {
     const activeStyle = { color: 'blue' };
     return (
       <div>
-        <div>
+        {/* <div>
           <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
           {' | '}
           <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
           {' | '}
           <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div>
+        </div> */}
         <Switch>
-          <Route exact path="/" component={HomePage} />
+          {/* <Route exact path="/" component={HomePage} />
           <Route path="/fuel-savings" component={FuelSavingsPage} />
           <Route path="/about" component={AboutPage} />
-          <Route component={NotFoundPage} />
+          <Route component={NotFoundPage} /> */}
+                {indexRoutes.map((prop, key) => {
+        return <Route to={prop.path} component={prop.component} key={key} />;
+      })}
         </Switch>
       </div>
     );
@@ -41,3 +44,4 @@ App.propTypes = {
 };
 
 export default hot(module)(App);
+
--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import './assets/styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 const store = configureStore();
 
