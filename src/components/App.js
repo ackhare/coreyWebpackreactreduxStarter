d32219c0694695a5505b275f51154ba82fdbd015
Khare <Chetan.Khare@CenturyLink.com>
2019-02-14 17:13:32 +0530

    added mdb react for datatables use

--- src/components/App.js
+++ src/components/App.js
@@ -15,21 +15,9 @@ import indexRoutes from "../routes/index.jsx";
 
 class App extends React.Component {
   render() {
-    const activeStyle = { color: 'blue' };
     return (
       <div>
-        {/* <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '}
-          <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
-          {' | '}
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div> */}
         <Switch>
-          {/* <Route exact path="/" component={HomePage} />
-          <Route path="/fuel-savings" component={FuelSavingsPage} />
-          <Route path="/about" component={AboutPage} />
-          <Route component={NotFoundPage} /> */}
                 {indexRoutes.map((prop, key) => {
         return <Route to={prop.path} component={prop.component} key={key} />;
       })}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,9 @@ import AboutPage from "../components/AboutPage";
 import FuelSavingsPage from "../components/containers/FuelSavingsPage";
 import NotFoundPage from "../components/NotFoundPage";
 var indexRoutes = [{ path: "/", name: "Home", component: Dashboard },
-{ path: "/fuel-savings", name: "Home", component: FuelSavingsPage },
-{ path: "/about", name: "Home", component: AboutPage },
-{ name: "Home", component: NotFoundPage  }
+{ path: "/fuel-savings", name: "fuel", component: FuelSavingsPage },
+{ path: "/about", name: "about", component: AboutPage },
+{ name: "404", component: NotFoundPage  }
 
 ];
 
